095d9a7af2addbd2624b115fbc929790ecf43a65
mekapeu <kahtchekmekapeu@gmail.com>
2022-11-03 14:24:58 -0700

    script that creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- creates database hbtn_0d_usa and table states
+-- both queries don't fail if db or user exist
+
+-- CREATE DB
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+-- CREATE TABLE
+CREATE TABLE IF NOT EXIST
