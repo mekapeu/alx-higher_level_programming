a14235f6a89ec79dfeddb2062c118de7c5c3a452
mekapeu <kahtchekmekapeu@gmail.com>
2022-12-16 17:01:26 -0800

    Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-    A script that fetches https://intranet.htbn.io/status
-"""
-
-
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        html = resp.read()
-
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
-        
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+if __name__ == "__main__":
+    fetcher()
+    
