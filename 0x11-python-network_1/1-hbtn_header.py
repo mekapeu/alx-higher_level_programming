cb297b7f5538a49dfefb71d7ec22a472aaf4e570
mekapeu <kahtchekmekapeu@gmail.com>
2022-12-16 17:14:22 -0800

    Python script that takes in a URL and an emai

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""
-    A script that sends a request to the URL and displays the
-    value of the X-Request-Id found in the response
-"""
-
-import sys
+"""Fetches header"""
 import urllib.request
+import sys
 
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header["X-Request-Id"])
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as resp:
-        print('{}'.format(resp.info().get('X-Request-id')))
-        
+    fetcher()
+    
